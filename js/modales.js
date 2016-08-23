dd9cde8c28800dca49be9e8e244878674f25e888
Cristian Restrepo <criscali91@gmail.com>
2016-08-23 14:52:43 -0500

    creacion archivo javascript para animacion de modal
    creacion archivo javascript en la carpeta js. para animacion de modal

---
+++ js/modales.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+	$('.modal-1').click(function(){
+		$('.modal1').show();
+	})
+
+	$('.icon-cross').click(function(){
+		$('.modal1').hide();
+	})
+
+	$('.modal-2').click(function(){
+		$('.modal2').show();
+	})
+
+	$('.icon-cross').click(function(){
+		$('.modal2').hide();
+	})
+
+	$('.modal-3').click(function(){
+		$('.modal3').show();
+	})
+
+	$('.icon-cross').click(function(){
+		$('.modal3').hide();
+	})
+
+	$('.modal-4').click(function(){
+		$('.modal4').show();
+	})
+
+	$('.icon-cross').click(function(){
+		$('.modal4').hide();
+	})
+
+})
